3c95dae8166b0b1f7289ed8aa16d8cb5e9a89b78
Axel Cabanat <axelcabanat@gmail.com>
2023-05-23 15:52:01 +0200

    add all_candidates_for_jobs_of_one_company query

--- queries/3_all_candidates_for_jobs_of_one_company.sql
+++ queries/3_all_candidates_for_jobs_of_one_company.sql
@@ -1,2 +1,9 @@
 # Écris une requêtes SQL qui retourne tous les candidats ayant répondu à 
 # au moins une offre d'une entreprise identifiée par son nom
+
+SELECT Candidate.*
+FROM Candidate
+INNER JOIN Candidacy ON Candidate.id = Candidacy.candidate_id
+INNER JOIN Offer ON Candidacy.offer_id = Offer.id
+INNER JOIN Company ON Offer.company_id = Company.id
+WHERE Company.name = 'WCS';
