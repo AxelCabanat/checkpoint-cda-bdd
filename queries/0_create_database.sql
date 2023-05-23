bacfe863ca0e250d7dd06b8dddd8e4bab97e452a
Axel Cabanat <axelcabanat@gmail.com>
2023-05-23 15:14:02 +0200

    add tables creation queries

--- queries/0_create_database.sql
+++ queries/0_create_database.sql
@@ -1,3 +1,40 @@
 # Suit les instruction du checkpoint, en utilisant https://www.dbdesigner.net/, créer un script
 # d'initialisation de BDD SQLite et copie colle son contenu ici
 # (tu dois notmalement avoir +/- 4 instructions CREATE TABLE, chaque instructions doit bien se terminer par un ";")
+
+CREATE TABLE Recruiter (
+	id integer PRIMARY KEY AUTOINCREMENT,
+	login varchar,
+	password varchar,
+	company_id integer
+);
+
+CREATE TABLE Candidate (
+	id integer PRIMARY KEY AUTOINCREMENT,
+	login varchar,
+	name varchar,
+	firstname varchar,
+	email varchar,
+	phone integer,
+	text text
+);
+
+CREATE TABLE Company (
+	id integer PRIMARY KEY AUTOINCREMENT,
+	name integer,
+	description integer
+);
+
+CREATE TABLE Offer (
+	id integer PRIMARY KEY AUTOINCREMENT,
+	title varchar,
+	description text,
+	city varchar,
+	company_id integer
+);
+
+CREATE TABLE Offer/Candidate (
+	id integer PRIMARY KEY AUTOINCREMENT,
+	offer_id integer,
+	candidate_id integer
+);
